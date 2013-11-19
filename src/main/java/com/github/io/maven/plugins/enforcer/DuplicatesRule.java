33d4993a6c94df344572984dff9c61665c83d2f7
Michael Sena <mikesena@gmail.com>
2013-11-20 09:36:12 +1100

    3 changes:  - Added 'excludes' to work alongside includes.  - Added integration tests for excludes  - Added Javadoc comments.

--- src/main/java/com/github/io/maven/plugins/enforcer/DuplicatesRule.java
+++ src/main/java/com/github/io/maven/plugins/enforcer/DuplicatesRule.java
@@ -38,34 +38,175 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
 
+/**
+ * Checks dependencies for classes previously included.
+ * 
+ * @author Michael Sena
+ * 
+ */
 public class DuplicatesRule implements EnforcerRule {
-    private Log log;
-    private MavenSession session;
-    private MavenProject project;
-    private final Map<String, List<Artifact>> problems;
     private final Map<String, Artifact> entries;
+    private final List<Pattern> excludePatterns;
+    private String[] excludes;
+    private final List<Pattern> includePatterns;
     private String[] includes;
-    private List<Pattern> includePatterns;
+    private Log log;
+    private final Map<String, List<Artifact>> problems;
+    private MavenProject project;
+    private MavenSession session;
 
+    /** Main constructor. */
     public DuplicatesRule() {
         problems = new HashMap<>();
         entries = new HashMap<>();
         includePatterns = new ArrayList<>();
+        excludePatterns = new ArrayList<>();
     }
 
+    @Override
     public void execute(final EnforcerRuleHelper helper) throws EnforcerRuleException {
         setup(helper);
         scanProject();
         processResult();
     }
 
-    private void scanProject() throws EnforcerRuleException {
-        for (final Artifact dependencyArtifact : project.getDependencyArtifacts()) {
-            final Artifact artifactToScan = session.getLocalRepository().find(dependencyArtifact);
-            processArtifact(artifactToScan);
+    @Override
+    public String getCacheId() {
+        return null;
+    }
+
+    @Override
+    public boolean isCacheable() {
+        return false;
+    }
+
+    @Override
+    public boolean isResultValid(final EnforcerRule arg0) {
+        return false;
+    }
+
+    /**
+     * Extracts out common Maven variables.
+     * 
+     * @param helper
+     *            Helper to extract from.
+     * @throws EnforcerRuleException
+     *             thrown on a general error. The error is wrapped in this exception, so the Maven build will fail
+     *             correctly.
+     */
+    protected void setup(final EnforcerRuleHelper helper) throws EnforcerRuleException {
+        log = helper.getLog();
+        try {
+            project = (MavenProject) helper.evaluate("${project}");
+            session = (MavenSession) helper.evaluate("${session}");
+        } catch (final ExpressionEvaluationException e) {
+            throw new EnforcerRuleException("Unable to lookup item " + e.getLocalizedMessage(), e);
+        }
+        if (includes != null) {
+            for (final String include : includes) {
+                includePatterns.add(Pattern.compile(include));
+            }
+        }
+        if (excludes != null) {
+            for (final String exclude : excludes) {
+                excludePatterns.add(Pattern.compile(exclude));
+            }
         }
     }
 
+    /**
+     * Get a list entry for the className.
+     * 
+     * @param className
+     *            (to retrieve)
+     * @return the entry
+     */
+    private List<Artifact> getProblemEntry(final String className) {
+        List<Artifact> problemEntry = problems.get(className);
+        if (problemEntry == null) {
+            problemEntry = new ArrayList<>();
+            problemEntry.add(entries.get(className));
+            problems.put(className, problemEntry);
+        }
+        return problemEntry;
+    }
+
+    /**
+     * Whether a specified class should be excluded.
+     * 
+     * @param className
+     *            (to check)
+     * @return class' inclusion
+     */
+    private boolean isExcluded(final String className) {
+        if (excludePatterns.size() == 0) {
+            return false;
+        } else {
+            for (final Pattern pattern : excludePatterns) {
+                if (pattern.matcher(className).matches()) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Whether a specified class should be included.
+     * 
+     * @param className
+     *            (to check)
+     * @return class' inclusion
+     */
+    private boolean isIncluded(final String className) {
+        if (includePatterns.size() == 0) {
+            return true;
+        } else {
+            for (final Pattern pattern : includePatterns) {
+                if (pattern.matcher(className).matches()) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Scans a Maven dependency, adding its entries to a store.
+     * 
+     * @param artifactToScan
+     *            Input artifact to process
+     * @throws EnforcerRuleException
+     *             thrown on a general error. The error is wrapped in this exception, so the Maven build will fail
+     *             correctly.
+     */
+    private void processArtifact(final Artifact artifactToScan) throws EnforcerRuleException {
+        log.debug("Scanning: " + artifactToScan.toString() + " (" + artifactToScan.getFile() + ")");
+
+        try (JarFile jar = new JarFile(artifactToScan.getFile())) {
+            final Enumeration<JarEntry> jarEntries = jar.entries();
+            while (jarEntries.hasMoreElements()) {
+                final JarEntry entry = jarEntries.nextElement();
+                if (entry.getName().endsWith(".class") && shouldBeChecked(entry.getName())) {
+                    if (!entries.containsKey(entry.getName())) {
+                        entries.put(entry.getName(), artifactToScan);
+                    } else {
+                        final List<Artifact> problemEntry = getProblemEntry(entry.getName());
+                        problemEntry.add(artifactToScan);
+                    }
+                }
+            }
+        } catch (final IOException e) {
+            throw new EnforcerRuleException("Unable to scan JAR file: " + artifactToScan.getFile().getPath(), e);
+        }
+    }
+
+    /**
+     * Displays the result of the rule, failing if necessary.
+     * 
+     * @throws EnforcerRuleException
+     *             thrown if the rule failed.
+     */
     private void processResult() throws EnforcerRuleException {
         if (problems.size() == 0) {
             return;
@@ -80,76 +221,32 @@ public class DuplicatesRule implements EnforcerRule {
         throw new EnforcerRuleException("Found: " + problems.size() + " violation(s).");
     }
 
-    private void setup(final EnforcerRuleHelper helper) throws EnforcerRuleException {
-        log = helper.getLog();
-        try {
-            project = (MavenProject) helper.evaluate("${project}");
-            session = (MavenSession) helper.evaluate("${session}");
-        } catch (ExpressionEvaluationException e) {
-            throw new EnforcerRuleException("Unable to lookup item " + e.getLocalizedMessage(), e);
+    /**
+     * Scan the Maven project, adding found entries to the list.
+     * 
+     * @throws EnforcerRuleException
+     *             thrown on a general error. The error is wrapped in this exception, so the Maven build will fail
+     *             correctly.
+     */
+    private void scanProject() throws EnforcerRuleException {
+        for (final Artifact dependencyArtifact : project.getDependencyArtifacts()) {
+            final Artifact artifactToScan = session.getLocalRepository().find(dependencyArtifact);
+            processArtifact(artifactToScan);
         }
+    }
 
-        if (includes != null) {
-            for (final String include : includes) {
-                includePatterns.add(Pattern.compile(include));
+    /**
+     * Whether a specified class is to be checked by the rule.
+     * 
+     * @param className
+     * @return
+     */
+    private boolean shouldBeChecked(final String className) {
+        if (isIncluded(className)) {
+            if (!isExcluded(className)) {
+                return true;
             }
         }
-    }
-
-    private void processArtifact(final Artifact artifactToScan) throws EnforcerRuleException {
-        log.debug("Scanning: " + artifactToScan.toString() + " (" + artifactToScan.getFile() + ")");
-
-        try (JarFile jar = new JarFile(artifactToScan.getFile())) {
-            final Enumeration<JarEntry> jarEntries = jar.entries();
-            while (jarEntries.hasMoreElements()) {
-                final JarEntry entry = jarEntries.nextElement();
-                if (entry.getName().endsWith(".class") && isIncluded(entry.getName())) {
-                    if (!entries.containsKey(entry.getName())) {
-                        entries.put(entry.getName(), artifactToScan);
-                    } else {
-                        final List<Artifact> problemEntry = getProblemEntry(entry.getName());
-                        problemEntry.add(artifactToScan);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new EnforcerRuleException("Unable to scan JAR file: " + artifactToScan.getFile().getPath(), e);
-        }
-    }
-
-    private boolean isIncluded(final String className) {
-        if (includePatterns.size() == 0) {
-            return true;
-        } else {
-            for (final Pattern pattern : includePatterns) {
-                if (pattern.matcher(className).matches()) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
-    private List<Artifact> getProblemEntry(final String className) {
-        List<Artifact> problemEntry = problems.get(className);
-        if (problemEntry == null) {
-            problemEntry = new ArrayList<>();
-            problemEntry.add(entries.get(className));
-            problems.put(className, problemEntry);
-        }
-        return problemEntry;
-    }
-
-    public boolean isCacheable() {
         return false;
-    }
-
-    public boolean isResultValid(EnforcerRule arg0) {
-        return false;
-    }
-
-    @Override
-    public String getCacheId() {
-        return null;
     }
 }
